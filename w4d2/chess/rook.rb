5e9a50b4096e1dcd024b08b4eb13b89dd1aaa186
Nick Pietrow <njpietrow@gmail.com>
2022-01-19 10:12:03 -0800

    created moves method in slideable class

--- w4d2/chess/rook.rb
+++ w4d2/chess/rook.rb
@@ -1,4 +1,12 @@
-class Rook
+require_relative "piece"
+require_relative "slideable"
 
+class Rook < Piece
+  include Slideable
 
+  def move_dirs
+    horizontal_dirs
+  end
+
+  # todo - symbol method.
 end
--- w4d2/chess/slideable.rb
+++ w4d2/chess/slideable.rb
@@ -21,7 +21,15 @@ module Slideable
   end
 
   def moves
+    potential_moves = move_dirs #returns up,right,down,left
+    possible_moves = []
+    
+    potential_moves.each do |dir|
+      row,col = dir
+      possible_moves += grow_unblocked_moves_in_dir(row,col)
+    end
 
+    possible_moves
   end
 
 
@@ -30,7 +38,10 @@ module Slideable
     #overwritten by subclass
   end
 
-  def grow_unblocked_moves_in_dir(dx, dy)
+  def grow_unblocked_moves_in_dir(change_in_row, change_in_col)
+    # iteratively increment values for row and/or col
+    # for each increment, check if piece is there/inbounds.
+    # return array of all possible positions
     
   end
 end
