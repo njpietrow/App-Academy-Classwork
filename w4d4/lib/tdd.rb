6baed893850e289c391a9d961473c83aa95a8584
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-01-20 17:44:26 -0500

    Finished two sum started my transpose

--- w4d4/lib/tdd.rb
+++ w4d4/lib/tdd.rb
@@ -3,3 +3,15 @@ def my_uniq(array)
   array.each {|ele| uniq_array << ele if !uniq_array.include?(ele)}
   uniq_array
 end
+
+class Array
+  def two_sum 
+    indicies = []
+    (0...self.length).each do |idx1|
+      (idx1 + 1...self.length).each do |idx2|
+        indicies << [idx1,idx2] if (self[idx1] + self[idx2]) == 0 
+      end 
+    end 
+    indicies
+  end
+end
--- w4d4/spec/tdd_spec.rb
+++ w4d4/spec/tdd_spec.rb
@@ -14,10 +14,31 @@ describe "TDD Tests" do
       it "returns indices of pairs that sum to zero" do
         expect(array.two_sum).to eq([[0, 4], [2, 3]])
       end
+      it "adds unique elements" do 
+        expect(array.two_sum).to_not include([1,1])
+      end
       context "if array is empty" do
         it "returns an empty array" do
           expect([].two_sum).to eq([])
         end
       end
+  end
+
+  describe "#my_transpose" do 
+    let(:array) {[[0, 1, 2], [3, 4, 5], [6, 7, 8]]} 
+    let(:tranposed) {[[0, 3, 6], [1, 4, 7], [2, 5, 8]]}
+    it "returns tranposed array" do 
+      expect(my_transpose(array)).to eq(tranposed)
+    end
+    it "returns the same amount of sub_arrays" do 
+      expect(my_transpose(array).length).to eq(tranposed.length)
+    end 
+    it "throws an error if the array is not square" do 
+      expect {my_transpose([[2],[1]])}.to raise_error("Array is not square")
+    end
+    it "does not modify orginal array" do 
+      expect(my_tranpose(array)).to_not be(array)
+      expect(array).to eq([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+    end
   end
 end
