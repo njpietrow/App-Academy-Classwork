613bc2a1f6083d99ba39248edb5a20e4e4c8583e
Nick Pietrow <njpietrow@gmail.com>
2022-01-21 10:52:34 -0800

    finished the first execution time exercises

---
+++ w4d5/execution_time.rb
@@ -0,0 +1,66 @@
+# my_min
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# my_min(list)  # =>  -5
+
+# Phase I
+# First, write a function that compares each element to every other element of the list. 
+# Return the element if all other elements in the array are larger.
+# What is the time complexity for this function?
+
+def comparison(array) # time complexity is O(n^2)
+  smallest = 0 
+  (0...array.length).each do |i|
+    (i...array.length).each do |j|
+      smallest = array[j] if array[j] <= array[i] && array[j] < smallest
+    end
+  end
+  smallest
+end
+
+# p comparison(list)
+
+# Phase II
+# Now rewrite the function to iterate through the list just once while keeping track of the minimum. 
+# What is the time complexity? 
+
+def my_min(array) # time complexity is O(n)
+  smallest = 0
+  array.each {|num| smallest = num if num < smallest}
+  smallest
+end
+
+# p my_min(list)
+
+
+
+
+# Largest Contiguous Sub-sum
+list1 = [2, 3, -6, 7, -6, 7]
+list2 = [-5, -1, -3]
+
+# Phase I
+# Write a function that iterates through the array and finds all sub-arrays using nested loops. 
+# First make an array to hold all sub-arrays. 
+# Then find the sums of each sub-array and return the max.
+# Discuss the time complexity of this solution.
+
+def iter_sub(array)
+  sub_arrays = []
+  (0...array.length).each do |i|
+    (i...array.length).each do |j|
+      sub_arrays << array[i..j].sum
+    end
+  end
+  return sub_arrays.sort[-1]
+end
+
+p iter_sub(list1)
+p iter_sub(list2)
+
+
+# Phase II
+# Let's make a better version. Write a new function using O(n) time with O(1) memory. 
+# Keep a running tally of the largest sum. To accomplish this efficient space complexity, 
+# consider using two variables. One variable should track the largest sum so far and another to 
+# track the current sum. We'll leave the rest to you.
