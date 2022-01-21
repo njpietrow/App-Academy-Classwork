2f493e30bdec0016f1b2bef5e18c75f3907395ec
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-01-20 20:29:58 -0500

    Continuing poker game

--- w4d4/poker/lib/deck.rb
+++ w4d4/poker/lib/deck.rb
@@ -2,4 +2,24 @@ require_relative "card"
 
 class Deck
 
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+    fill_cards
+  end
+
+
+  private 
+  def fill_cards 
+    suits = %w(Hearts Spades Clubs Diamonds)
+
+    suits.each do |suit|
+      (2..14).each do |value|
+        cards << Card.new(value, suit)
+      end
+    end
+
+  end
+
 end
--- w4d4/poker/spec/deck_spec.rb
+++ w4d4/poker/spec/deck_spec.rb
@@ -13,6 +13,14 @@ describe Deck do
     it "should have 52 cards" do
       expect(deck.cards.length).to eq(52)
     end
-    
   end
+
+  describe "#shuffle!" do 
+    it "shuffles cards in deck" do
+      previous_state = deck.cards.dup
+      deck.shuffle!
+      expect(deck.cards).to_not eq(previous_state)
+    end
+  end
+
 end
