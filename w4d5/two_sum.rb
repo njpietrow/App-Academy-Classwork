aab110e3bd502fc94853de10f2bc04c5becff805
JonJWong <wongjonathan01@gmail.com>
2022-01-21 15:18:43 -0800

    finished two_sum

--- w4d5/two_sum.rb
+++ w4d5/two_sum.rb
@@ -34,5 +34,27 @@ def okay_two_sum?(arr, target_sum)
   false
 end
 
+# arr = [1000, 0, 1, 5, 7 , 8, 9, -5, -3]
+# p okay_two_sum?(arr, -8)
+
+# Finally, it's time to bust out the big guns: a hash map. Remember, a hash map has O(1) #set and O(1) #get,
+# so you can build a hash out of each of the n elements in your array in O(n) time.
+# That hash map prevents you from having to repeatedly find values in the array; now you can just look them up instantly.
+
+# See if you can solve the two_sum? problem in linear time now, using your hash map.
+
+# Once you're finished, make sure you understand the time complexity of your solutions.
+# Then call over your TA and show them what you've got. Defend to them why
+# each of your solutions has the time complexity you claim it does.
+
+def two_sum(arr, target_sum)
+  h = {}
+  arr.each do |num|
+    return true if h.key(num)
+    h[num] = target_sum - num
+  end
+  return false
+end
+
 arr = [1000, 0, 1, 5, 7 , 8, 9, -5, -3]
-p okay_two_sum?(arr, -8)
+p two_sum(arr, 1009)
