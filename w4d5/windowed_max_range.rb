2fa33a3106a937a8ae64b03f9ef83fde679688d4
Nick Pietrow <njpietrow@gmail.com>
2022-01-21 17:00:14 -0800

    made edits to stackqueue class

--- w4d5/windowed_max_range.rb
+++ w4d5/windowed_max_range.rb
@@ -68,19 +68,70 @@ class MyQueue
   end
 
   def empty?
-    return store.empty?
+    return store.length == 0
   end
 
   private
-  attr_accessor :store
+  attr_reader :store
 end
 
 class MyStack
-  
+  def initialize
+    @store = []
+  end
+
+  def peek
+    return store[-1]
+  end
+
+  def pop
+    return store.pop
+  end
+
+  def push(ele)
+    store.push(ele)
+    return nil
+  end
+
+  def size
+    return store.length
+  end
+
+  def empty?
+    return store.length == 0
+  end
+
+  attr_reader :store
 end
 
 class StackQueue
+  def initialize
+    @queue = [MyStack.new,MyStack.new]
+  end
 
+  def size
+    queue[0].length + queue[1].length
+  end
+
+  def empty?
+    queue[0].empty? && queue[1].empty?
+  end
+
+  def enqueue(ele)
+    until queue[1].empty?
+      queue[0].push(queue[1].pop)
+    end
+    queue[1].push(ele)
+  end
+
+  def dequeue
+    until queue[0].empty?
+      queue[1].push(queue[0].pop)
+    end
+    queue[1].pop
+  end
+
+  attr_reader :queue
 end
 
 class MinMaxStack
