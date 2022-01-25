423f670705c9ecc248a1064179a03720d96149bf
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-01-24 20:03:31 -0500

    Add HashMap class

--- w5d1/lib/p05_hash_map.rb
+++ w5d1/lib/p05_hash_map.rb
@@ -2,6 +2,7 @@ require_relative 'p04_linked_list'
 
 class HashMap
   attr_accessor :count
+  include Enumerable
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -9,15 +10,28 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if @count >= num_buckets
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    return nil unless include?(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    return nil unless include?(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
   def each
@@ -41,9 +55,17 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(2*num_buckets) {LinkedList.new}
+    @store.each do |bucket|
+      bucket.each do |ele|
+        new_store[ele.key.hash % (2 * num_buckets)].append(ele.key, ele.val)
+      end
+    end
+    @store = new_store
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
