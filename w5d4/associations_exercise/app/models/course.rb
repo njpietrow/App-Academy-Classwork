73f6eb77782a6e68d2fd19ecbc5f1d691ec49de6
Nick Pietrow <njpietrow@gmail.com>
2022-01-27 14:56:41 -0800

    finished model associations for User Enrollments Cours

--- w5d4/associations_exercise/app/models/course.rb
+++ w5d4/associations_exercise/app/models/course.rb
@@ -10,4 +10,26 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+  belongs_to :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course,
+    optional: true
+
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+  
 end
