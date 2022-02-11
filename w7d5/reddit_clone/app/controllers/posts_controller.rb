054fb99300e400f81ccd75b7cb9f91ea77bf0865
Nick Pietrow <njpietrow@gmail.com>
2022-02-11 15:38:58 -0800

    created feature to create posts

--- w7d5/reddit_clone/app/controllers/posts_controller.rb
+++ w7d5/reddit_clone/app/controllers/posts_controller.rb
@@ -1,11 +1,25 @@
 class PostsController < ApplicationController
   def show
+    @post = Post.find_by(id: params[:id])
+    render :show
   end
 
   def new
+    @post = Post.new
+    @sub = Sub.find_by(id: params[:sub_id])
+    render :new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.sub_id = params[:sub_id]
+    @post.author_id = current_user.id
+    if @post.save
+      redirect_to sub_url(@post.sub_id)
+    else
+      flash[:errors] = @post.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +29,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :url, :content)
   end
 end
--- w7d5/reddit_clone/config/routes.rb
+++ w7d5/reddit_clone/config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
 
   resource :session
 
-  resources :subs, except: [:destroy]
+  resources :subs, except: [:destroy] do
+    resources :posts
+  end
+
 end
