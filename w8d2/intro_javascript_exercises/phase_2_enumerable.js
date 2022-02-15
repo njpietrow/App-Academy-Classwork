980c89ec0dd078a33b2c99b2dfb516a7774ffcdc
Nick Pietrow <njpietrow@gmail.com>
2022-02-15 15:18:33 -0800

    created myMap and myReduce

--- w8d2/intro_javascript_exercises/phase_2_enumerable.js
+++ w8d2/intro_javascript_exercises/phase_2_enumerable.js
@@ -5,3 +5,36 @@ Array.prototype.myEach = function (callback) {
 }
 
 // console.log([1,2,3].myEach(console.log));
+
+Array.prototype.myMap = function (callback) {
+    let mapped = [];
+    this.myEach(function(ele) {
+        mapped.push(callback(ele));
+    });
+    return mapped;
+}
+
+// console.log([1,2,3].myMap(function(ele) {
+//     return ele*2;
+// }));
+
+Array.prototype.myReduce = function(callback , initialValue) {    
+    let array = this;
+    if (initialValue === undefined){
+        initialValue = this[0];
+        array = array.slice(1);
+    }
+    let total = initialValue;
+    array.myEach(function(ele){
+        total = callback(total, ele);
+    });
+    return total;
+}
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+  })); // 6
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+  }, 25)); // 150
