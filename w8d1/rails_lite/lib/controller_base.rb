628bb3a5e492e0ecdd9648411c375895a13f0475
David Domingo <ddoming824@gmail.com>
2022-02-14 17:18:52 -0800

    finished p04

--- w8d1/rails_lite/lib/controller_base.rb
+++ w8d1/rails_lite/lib/controller_base.rb
@@ -1,7 +1,9 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
+require 'byebug'
 
 class ControllerBase
   attr_reader :req, :res, :params
@@ -23,6 +25,7 @@ class ControllerBase
     @res.status = 302
     @res['Location'] = url
     @already_built_response = true
+    session.store_session(@res)
     nil
   end
 
@@ -34,16 +37,26 @@ class ControllerBase
     @res.write(content)
     @res['Content-Type'] = content_type
     @already_built_response = true
+    session.store_session(@res)
     nil
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.to_s.underscore
+    begin_path = File.dirname(__FILE__).split('/')[0...-1]   
+    end_path = "views/#{controller_name}/#{template_name}.html.erb"
+    full_path = File.join( *begin_path, end_path)
+    file = File.open(full_path)
+    content = file.read
+    erb_content = ERB.new(content).result(binding)
+    render_content(erb_content)
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
--- w8d1/rails_lite/lib/session.rb
+++ w8d1/rails_lite/lib/session.rb
@@ -4,16 +4,28 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    # req.cookies("_rails_lite_app")
+    cookie = req.cookies['_rails_lite_app']
+    if cookie
+      @cookie_value = JSON.parse(cookie)
+    else
+      @cookie_value = {}
+    end
   end
-
+  
   def [](key)
+    @cookie_value[key]
   end
-
+  
   def []=(key, val)
+    @cookie_value[key] = val
   end
-
+  
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    # res = Rack::Response.new
+    res.set_cookie('_rails_lite_app', {path:'/', value: @cookie_value.to_json})
+    res.finish
   end
 end
